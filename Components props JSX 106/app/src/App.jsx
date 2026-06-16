a18b7af8b18032159b1597c8f75b0bb2d852ae2a
Debashree <debashreee87@gmail.com>
2026-06-16 17:40:47 +0530

    Used Vite for react APP

---
+++ Components props JSX 106/app/src/App.jsx
@@ -0,0 +1,18 @@
+import Footer from "./components/Footer"
+import Navbar from "./components/Navbar"
+
+
+function App() {
+  
+  return (
+    <>
+      <Navbar/>
+      <main>  
+        This is the main content of the page.
+      </main>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ Components props JSX 106/app/src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className='card'>
+        <h1>Title of the Card</h1>
+      
+    </div>
+  )
+}
+
+export default Card
