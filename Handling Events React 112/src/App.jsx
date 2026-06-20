dd7148d438abd0a022fa9853816c47070245a8be
Debashree <debashreee87@gmail.com>
2026-06-20 15:51:00 +0530

    Handling input event: handleChange . Completed Tutorial 112 '

--- Handling Events React 112/src/App.jsx
+++ Handling Events React 112/src/App.jsx
@@ -35,8 +35,8 @@ const handleChange = (e) => {
         I am a red div
       </div>
 
-      <input type="text" name='email' value={form.email} onChange={handleChange}/>
-      <input type="text" name='phone' value={form.phone} onChange={handleChange}/>
+      <input type="text" name='email' value={form.email?form.email:""} onChange={handleChange}/>
+      <input type="text" name='phone' value={form.phone?form.phone:""} onChange={handleChange}/>
 
     </>
   )
