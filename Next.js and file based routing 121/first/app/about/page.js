2e6d9bdd1fa376020d101848b250bd87be17b39e
Debashree <debashreee87@gmail.com>
2026-06-23 20:48:02 +0530

    File based  routing using @ directly in layout.js

--- Next.js and file based routing 121/first/app/about/page.js
+++ Next.js and file based routing 121/first/app/about/page.js
@@ -1,10 +1,17 @@
-import { discoverValidationDepths } from "next/dist/server/app-render/instant-validation/instant-validation";
-import Image from "next/image";
+import React from 'react'
 
-export default function Home() {
+const page = () => {
   return (
     <div>
-      <div> I am About</div>
+      <h1 className='text'>Problem solved by Next.js .....</h1>
+      <ul>
+       <li>Solutions for Full Stack</li>
+       <li>File based routing usng @ </li>
+       <li>Additional features: router from next/navigation</li>
+       <li>Optimized Rendering</li>
+      </ul>
     </div>
-  );
+  )
 }
+
+export default page
