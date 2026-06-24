b6a5db50ce4838806bcdef3a34d1a4caea763bbd
Debashree <debashreee87@gmail.com>
2026-06-24 18:22:18 +0530

    image fill and Contain in Next.js Tutorial 123 completed

--- Script and link and Image Next,js 123/my-app/app/page.js
+++ Script and link and Image Next,js 123/my-app/app/page.js
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <div className="containe my-10">
-      <img className="mx-auto" src="https://gratisography.com/wp-content/uploads/2025/05/gratisography-moon-robot-800x525.jpg" alt="" />
+      <img className="mx-auto" fill={true} src="https://gratisography.com/wp-content/uploads/2025/05/gratisography-moon-robot-800x525.jpg" alt="" />
    </div>
   );
 }
