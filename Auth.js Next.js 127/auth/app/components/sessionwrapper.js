b732d11d0ed2c2a27fef2d4b4c2c499ae9b03d51
Debashree <debashreee87@gmail.com>
2026-06-26 21:30:58 +0530

    Completed tutorial 127

---
+++ Auth.js Next.js 127/auth/app/components/sessionwrapper.js
@@ -0,0 +1,11 @@
+"use client"
+import { SessionProvider } from "next-auth/react"
+
+
+const sessionwrapper = ({children}) => {
+  return (
+   <SessionProvider>{children}</SessionProvider>
+  )
+}
+
+export default sessionwrapper
