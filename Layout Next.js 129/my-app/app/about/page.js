36226bf603e9df6b31abc5d5d1b6366312b1d762
Debashree <debashreee87@gmail.com>
2026-06-28 16:48:35 +0530

    nested Layouts in Next.js to put anything over pages tutorial 129  completed

---
+++ Layout Next.js 129/my-app/app/about/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      Facebook iss veryyy good app to connect with the persons we belongs to and also new friends
+    </div>
+  )
+}
+
+export default page
---
+++ Layout Next.js 129/my-app/app/admin/layout.js
@@ -0,0 +1,27 @@
+import { Geist, Geist_Mono } from "next/font/google";
+// import Navbar from "./Components/Navbar";
+// import Footer from "./Components/Footer";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Admin: Faceboook  App",
+  description: "connect with friends and the world around you",
+};
+
+export default function AdminLayout({ children }) {
+  return (
+   <>
+   <span>Admin Navbar</span>
+        {children}
+    </>
+  );
+}
