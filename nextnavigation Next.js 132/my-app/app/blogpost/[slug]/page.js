fbfc4a0e4888a260a36c1de6c06a5e3d15e55fbc
Debashree <debashreee87@gmail.com>
2026-06-28 18:05:23 +0530

    Tutorial 132 is completed

--- nextnavigation Next.js 132/my-app/app/blogpost/[slug]/page.js
+++ nextnavigation Next.js 132/my-app/app/blogpost/[slug]/page.js
@@ -2,12 +2,17 @@
 
 import React from 'react'
 import  {useParams} from 'next/navigation'
+import {useRouter} from 'next/navigation'
 
 const page = () => {
     const params = useParams()
+    const router = useRouter()
   return (
     <div>
       {params.slug}
+
+      <button type="button" onClick={() => router.push('/dashboard')}>Dashboard</button>
+
     </div>
   )
 }
