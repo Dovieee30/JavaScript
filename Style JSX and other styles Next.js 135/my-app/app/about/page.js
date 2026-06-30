c1e04539a7d1f67e4cd561eaa950ea56ae8ec9c7
Debashree <debashreee87@gmail.com>
2026-06-30 19:24:19 +0530

    Tutorial 135 Completed learned style jsx : used in very specfic part of code wher we want to aplly css and global to create the css globally

---
+++ Style JSX and other styles Next.js 135/my-app/app/about/page.js
@@ -0,0 +1,30 @@
+"use client"
+
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+        
+    <div classname="container">
+      <h1>This is About Me Page</h1>
+      <p>Hey I am a sweet female </p>
+
+
+      <style jsx global>{`
+        .container{
+        background-color: mediumvioletred;
+        }
+       ` }
+      </style>
+    </div>
+
+    <div className="container">
+        Heyyyyyyyyy
+    </div>
+
+    </div>
+  )
+}
+
+export default About
