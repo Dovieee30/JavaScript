4491a395e4ca7398de6749a5c2831eb5acf1c7bf
Debashree <debashreee87@gmail.com>
2026-07-29 23:09:12 +0530

    Promises Completed

--- Promises Namaste JS/index.js
+++ Promises Namaste JS/index.js
@@ -1,29 +1,21 @@
+
 const cart = ["shoes", "shirt", "hat"];
 
-//Async operations using callback
+//How we can do Async operations using callback
 //create a order and give an orderID for Payment 
-//ISSUE: Inversion of control - gave the control of a program to other part of code(control of payment to create order)
+//ISSUE 1: Inversion of control - gave the control of a program to other part of code(control of payment to create order)
    
 createOrder(cart, function(orderID){
     proceedToPayment(orderID);
 }); 
 
-//Callback hell - Pyramid Of DOM 
-//Code starts growing horizonatly & hard to maintain.
-
-createOrder(cart, function(orderID){
-    proceedToPayment(orderID, function(paymentInfo){
-        ordersummary(paymentInfo, function(){
-            updatebalance();
-        });
-    });
-}); 
 
 
+//SOLUTION 1: Promises - Inversion of control
 
-//SOLUTION: Promises
 //Promise is an empty object {} which has a data, that is returned from the createOrder(cart) api , this api is a async operation.
 //Promise is an object represents eventual completion or failure of an asynchronous Operartion.
+
 
 const promise = createOrder(cart);
 
@@ -37,3 +29,40 @@ promise.then(function(){
 //Attach a callback function to promise using .then ,when data retrives in the promise object this callback function automatically executes.
 
 //conclusion : previously we were passing the callback function , now we are attaching the callback function. we have the control 
+
+
+
+
+//ISSUE 2: Callback hell - Pyramid Of DOM 
+//Code starts growing horizonatly & hard to maintain.
+
+createOrder(cart, function(orderID){
+    proceedToPayment(orderID, function(paymentInfo){
+        ordersummary(paymentInfo, function(){
+            updatebalance();
+        });
+    });
+}); 
+
+//SOLUTION 2: Promises - Callback hell
+//It Gets in meaningful chain fashion
+//you have to return the promise once again in chain - if not , you may loose some data.
+
+createOrder(cart)
+.then(function(orderID){
+    return proceedToPayment(orderID);
+})
+.then(function(paymentInfo){
+    return ordersummary(paymentInfo);
+})
+.then(function(paymentInfo){
+    return updatebalance(paymentInfo);
+})
+
+//OR you can just use Arrow function as it doesn't requires the promise to be return.
+
+createOrder(cart)
+.then((orderID) => proceedToPayment(orderID))
+.then((paymentInfo) => ordersummary(paymentInfo))
+.then((paymentInfo) => updatebalance(paymentInfo))
+
--- Promises Namaste JS/index1.js
+++ Promises Namaste JS/index1.js
@@ -6,12 +6,15 @@ console.log(user);
 //console Output
 //Promise {<pending>}[[Prototype]]: Promise[[PromiseState]]: "fulfilled"[[PromiseResult]]: Response
 
+//3 states of promises
 //pending : take some time to get the data 
 //fulfilled : data is fetched successfully
+//rejected : 
 
 //attaching
 user.then(function(data){
     console.log(data);
 });
 
-//Promises Objects are immutable (cannot be changed) after fullfilled.
+//Promises Objects are immutable (cannot be changed) after fullfilled or resolved.
+//Promise can be resolved just once (runs atleast once) - gives rust to the user
