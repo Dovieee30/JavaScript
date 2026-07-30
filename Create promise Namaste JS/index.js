4085f8f988b5b646c703f5d6c59ed14cb117e6f3
Debashree <debashreee87@gmail.com>
2026-07-30 22:57:14 +0530

    using resolve and reject aruguements in promise

--- Create promise Namaste JS/index.js
+++ Create promise Namaste JS/index.js
@@ -1,9 +1,41 @@
 const cart = ["shoes", "pants", "kurta"]
 
-const promise = createOrder(cart); //orderId
+const promise = createOrder(cart); //returns orderId
+console.log(promise);
 
-promise.then(function(){
-    proccedPayment(orderId);
-})
+promise.then(function(orderId){
+    console.log(orderId);
+    // proccedPayment(orderId);
+});
 
-//How to create promise that is how to createOrder(cart) function that returns a promise.
+
+//How to create promise? that is how to createOrder(cart) function that returns a promise.
+
+function createOrder(cart){
+    const pr = new Promise(function(resolve, reject){      
+        //validateCart
+        //createOrder
+        //orderId
+
+        if(!validateCart(cart)){
+            const err = new Error("Cart is not valid")
+            reject(err);
+        }
+
+        //createOrder 
+        const orderId = "1234"
+        if(orderId){
+            setTimeout(function(){
+                resolve(orderId);
+            }, 5000)
+        }
+
+    });
+
+    return pr;
+}
+
+//we have to write a function of validateCart to use it previously.
+function validateCart(cart){
+    return true;
+}
