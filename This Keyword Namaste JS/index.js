589461e33bd37244f19e887978c2b50457311af1
Debashree <debashreee87@gmail.com>
2026-08-05 22:55:12 +0530

    this substitution

--- This Keyword Namaste JS/index.js
+++ This Keyword Namaste JS/index.js
@@ -1,7 +1,26 @@
+"use strict";
+
 //this in Global space 
-console.log(this); //represents the globalobject - window in browser.
+
+console.log(this); 
+/* represents the globalobject - window in browser.
+ globalobject ? - where JS runs in the browser is window object. 
+ In nodejs it is global object. */
+
+
 
 //this inside the function
-function x() {
 
+function x() {
+    /* The value depends on Strict , non-strict mode.
+       strict Mode : value Undefined
+       non-strict Mode : value window */
+    console.log(this);
 }
+x();
+
+/* WHY?? : The value depends on Strict , non-strict mode. 
+   => "this substitution" : If the value of this keyword is undefined or null,
+       then this keyword will be replaced by global object,
+       only in non-strict mode.
+*/
